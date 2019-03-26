d69e75e8b31f539568c76af9d83ae921a2d0d16c
dreamstart <563681252@qq.com>
2019-03-26 12:32:48 +0800

    Update split_url_flie.cpp

--- solve/split_url_flie.cpp
+++ solve/split_url_flie.cpp
@@ -1,11 +1,18 @@
+/*
+ *
+ * Split large files into small files
+ *
+ */
+
+
 #include <fstream>
 #include <cstring>
 #include <iostream>
 #include "split_url_flie.h"
 using namespace std;
-const int flie_nums = 200; //The url flie numbers
+const int file_nums = 200; //The url subfile numbers
 
-int Hash(const string &s)
+int Hash(const string &s)  //hash函数
 {
 	const int base = 107;
 	int res = 0;
@@ -16,15 +23,15 @@ int Hash(const string &s)
 	return res;
 }
 
-void flie(const string &flie_name)
+void split_file(const string &file_name) //将大文件按值写入小文件
 {
 	ifstream turn_on;
-	turn_on.open(flie_name);
+	turn_on.open(file_name);
 	string s;
 	while(turn_on >> s) 
 	{
 		int hash_s = Hash(s);
-		hash_s = hash_s % flie_nums;
+		hash_s = hash_s % file_nums;
 		ofstream write_in;
 		write_in.open("hash_url_" + to_string(hash_s) + ".txt", ios::app);
 		write_in << s << endl;
@@ -34,11 +41,9 @@ void flie(const string &flie_name)
 }
 
 //unit testing
-/*
 int main()
 {
-	string flie_name = "data.txt";
-	flie(flie_name);
+	string file_name = "data.txt";
+	split_file(file_name);
 	return 0;
 }
-*/
