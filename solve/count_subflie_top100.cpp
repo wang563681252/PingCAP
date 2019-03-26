dc98dc7ad14569c919036a32dc066ac064857071
wang563681252 <563681252@qq.com>
2019-03-26 12:17:34 +0800

    Create count_subflie_top100.cpp

---
+++ solve/count_subflie_top100.cpp
@@ -0,0 +1,16 @@
+/*
+ * 
+ * Top100 calculates the number of URL occurrences in each subfile
+ *
+ */
+
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void count_top100()
+{
+	string 
+}
