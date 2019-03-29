356416e14aa730147a4b58e32b1e7a42e4b66eb9
wang563681252 <563681252@qq.com>
2019-03-29 17:12:44 +0800

    Update split_url_file.cpp

--- solve/split_url_file.cpp
+++ solve/split_url_file.cpp
@@ -14,6 +14,7 @@
 #include <map>
 #include "split_url_file.h"
 using namespace std;
+const int MAX_file = 3e6;
 
 int Hash(const string &s)  //hash function
 {
@@ -26,9 +27,15 @@ int Hash(const string &s)  //hash function
 	return res;
 }
 
-void split_file(const string file_name, const int &file_nums) //Write large files to small files by hash value
+int split_file(const string file_name, const int &file_nums) //Write large files to small files by hash value
 {
-	map<int, int> flag; //Mark how many urls are stored in this subfile
+	map<int, int> flag; //Output file with hash value
+	map<int, int> file_size; //Record how many urls the file stores
+	//Initialize the hash value location
+	for(int i = 1; i <= file_nums; i++) 
+	{
+		flag[i] = i;
+	}
 
 	ifstream turn_on;
 	turn_on.open(file_name);
@@ -39,11 +46,19 @@ void split_file(const string file_name, const int &file_nums) //Write large file
 	}
 
 	//Write the file by value after the s string hash
+	int tot_hash_file = file_nums; //Record the number of file expansions
 	string s;
 	while(turn_on >> s) 
 	{
+		//Calculate the hash value and calculate which folder to put in
 		int hash_s = Hash(s);
 		hash_s = hash_s % file_nums + 1;
+		if(file_size[flag[hash_s]] >= MAX_file) {
+			flag[hash_s] = tot_hash_file + 1;
+			tot_hash_file++;
+		}
+		file_size[flag[hash_s]]++; 
+
 
 		//output
 		ofstream write_in;
@@ -52,7 +67,7 @@ void split_file(const string file_name, const int &file_nums) //Write large file
         write_in.close();
 	}
 	turn_on.close();
-	return ;
+	return tot_hash_file;
 }
 
 //unit testing
