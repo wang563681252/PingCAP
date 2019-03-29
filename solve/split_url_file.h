f264fc7f0ee5da6696cb91e2d3f9216c98513c96
wang563681252 <563681252@qq.com>
2019-03-29 17:12:37 +0800

    Update split_url_file.h

--- solve/split_url_file.h
+++ solve/split_url_file.h
@@ -15,6 +15,6 @@
 using namespace std;
 
 int Hash(const string &s); //hash function
-void split_file(const string file_name, const int &file_nums); //Write large files to small files by value
+int split_file(const string file_name, const int &file_nums); //Write large files to small files by value
 
 #endif
